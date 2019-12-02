20f2c8f2a48a7c7a985fd9c301aefc05cb9d9e38
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-02 21:30:15 +0500

    Create state.js

---
+++ static/js/cabinet/state.js
@@ -0,0 +1,43 @@
+const initialState = {
+    modalType:'add', 
+    currentId:0,
+
+    orders:[
+        {   
+            date:'Минкультуры',
+            order:'	http://mkrf.ru/',
+            comment:''
+        }, 
+        {
+            date:'Минпросвещения',
+            order:'http://edu.gov.ru/	',
+            comment:''
+        },
+        
+        {
+            date:'Минкомсвязь',
+            order:' http://minsvyaz.ru/ru/	',
+            comment:''
+        },
+        {
+            date:'Минэкономразвития',
+            order:'http://economy.gov.ru/minec/main	',
+            comment:''
+        },
+        {
+            date:'Роскомнадзор',
+            order:'	http://rkn.gov.ru/	',
+            comment:''
+        },
+        {
+            date:'Росмолодёжь',
+            order:'	http://fadm.gov.ru',
+            comment:''
+        },
+        {
+            date:'Bk',
+            order:'https://vk.com	',
+            comment:''
+        }
+    ]
+}
